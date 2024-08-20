fa5d93245c364ddcdb90eb324680450279a888af
Marat Akbar Boyev <maratboyev@outlook.com>
2024-08-20 18:11:48 +0000

    Import blog view, add path to url and add allowed hosts

--- catablog/settings.py
+++ catablog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9yc#(n(4z6*xlh#dn-6dn6@p8+ko5)bil!_lmtvt+58@q^$+zk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-mabswe-pp4-mx96bk2rdt3.ws.codeinstitute-ide.net']
 
 
 # Application definition
