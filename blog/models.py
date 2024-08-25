d333f30810dde31b0da70ff291958cf0bc64c9e9
Marat Akbar Boyev <maratboyev@outlook.com>
2024-08-25 11:11:18 +0000

    Add migration to the models

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-    content = models.TextField()
+    content = models.TextField(default="Content will be updated.")
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
