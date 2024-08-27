340c57a5f53c8dbe4f6cce25b5999d53ce36efc7
Marat Akbar Boyev <maratboyev@outlook.com>
2024-08-27 18:38:17 +0000

    Tidy up Post and Comment section, add author to comments

--- blog/models.py
+++ blog/models.py
@@ -8,13 +8,10 @@ STATUS = ((0, "Draft"), (1, "Published"))
 # Post model for posts
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     content = models.TextField(default="")
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
@@ -22,8 +19,8 @@ class Post(models.Model):
 # Post model for comments
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_comments")
     name = models.CharField(max_length=80)
-    email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
