09e00d3b20244333e404df227ed37d09b5fa23af
Marat Akbar Boyev <maratboyev@outlook.com>
2024-08-27 17:51:54 +0000

    Add urls to blog, Add home.html and tidy up a bit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def my_blog(request):
-    return HttpResponse("Hello, Blog!")
+    return render(request, 'home.html', {})
