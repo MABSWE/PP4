b46b6171d0fec5a7a2740287240bf0d5feed62ec
Marat Akbar Boyev <maratboyev@outlook.com>
2024-09-03 21:38:01 +0000

    Fix url import and Add post template with title, author, and body content

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-# from . import views
-from .views import HomeView, ArticleDetailView
+from . import views  # Importera views-modulen här
 
 urlpatterns = [
-    # path('', views.home, name="home"),
-    path('', HomeView.as_view(), name="home"),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('', views.HomeView.as_view(), name='home'),  # Exempel på en URL
+    path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='article-detail'),
+    # Lägg till andra URL-mönster här
 ]
