a7b6ef8322a339e4db9cf62b6add71e1245a9069
Marat Akbar Boyev <maratboyev@outlook.com>
2024-09-03 21:42:53 +0000

    Tidy up a bit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views  # Importera views-modulen här
+from . import views  
+
+# Importera views-modulen här
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),  # Exempel på en URL
+    path('', views.HomeView.as_view(), name='home'),
     path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='article-detail'),
-    # Lägg till andra URL-mönster här
 ]
