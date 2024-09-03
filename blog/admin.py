6383e0c8d7e20fb98d754e6dba3e6b3e9f1953c3
Marat Akbar Boyev <maratboyev@outlook.com>
2024-09-03 20:59:05 +0000

    Add missing slug for making admin comment. Make migrations

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Post, Comment
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'created_on')
+    prepopulated_fields = {'slug': ('title',)} 
+
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
