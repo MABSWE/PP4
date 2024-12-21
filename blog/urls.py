d3686a50b9824a54ef480066e09ca419fa6c434c
Marat Akbar Boyev <maratboyev@outlook.com>
2024-12-21 14:35:58 +0000

    Add about, login, blog, register, contact pages

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,11 @@ from . import views
 # Importera views-modulen här
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.home, name='home'),  # Welcome page
+    path('blog/', views.BlogView.as_view(), name='blog'),  # Blog
     path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='article-detail'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('contact/', views.contact, name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,34 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
 
-# Create your views here.
-# def home(request):
-#     return render(request, 'home.html', {})
+# Home View (Welcome)
+def home(request):
+    return render(request, 'home.html')
 
-class HomeView(ListView):
+# Blog Page
+class BlogView(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = 'blog.html'
     context_object_name = 'posts'
 
+# Detail view
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'detail_view.html'
     context_object_name = 'post'
+
+# About Page
+def about(request):
+    return render(request, 'about.html')
+
+# Login Page
+def login_view(request):
+    return render(request, 'login.html')
+
+# Register Page
+def register(request):
+    return render(request, 'register.html')
+
+# Contact Page
+def contact(request):
+    return render(request, 'contact.html')
