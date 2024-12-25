decb577ca02c9e75800db0c4b891fcf44e4df856
Marat Akbar Boyev <maratboyev@outlook.com>
2024-12-25 16:59:27 +0000

    Add code to register page and update url and views

--- blog/urls.py
+++ blog/urls.py
@@ -7,10 +7,9 @@ urlpatterns = [
     path('', views.home, name='home'),  # Welcome page
     path('blog/', views.BlogView.as_view(), name='blog'),  # Blog
     path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='article-detail'),
-    path('about/', views.about, name='about'),
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register, name='register'),
-    path("contact/", views.contact_view, name="contact"),
-    path("contact-success/", views.contact_success_view, name="contact-success"),
+    path('about/', views.about, name='about'),  # About Page
+    path('login/', views.login_view, name='login'),  # Login Page
+    path('register/', views.register, name='register'),  # Register Page
+    path("contact/", views.contact_view, name="contact"),  # Contact Page
+    path("contact-success/", views.contact_success_view, name="contact-success"),  # Contact Success Page
 ]
-
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.views.generic import ListView, DetailView
-from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from .models import Post
 from .forms import ContactForm  
 
 # Home View (Welcome)
@@ -31,7 +33,18 @@ def login_view(request):
 
 # Register Page
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account created for {username}! You can now log in.")
+            return redirect('login')
+        else:
+            messages.error(request, "Please correct the errors below.")
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
 # Contact Page
 def contact_view(request):
